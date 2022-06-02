724d75b8cc0f4d707bf75a3c6a0e1e6df885334c
maaluoliv <84928556+maaluoliv@users.noreply.github.com>
2022-06-01 21:52:42 -0300

    Add files via upload

---
+++ src/ManipuladorArquivo.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class ManipuladorArquivo {
+    public static void leitor(String path) throws IOException {
+        BufferedReader buffRead = new BufferedReader(new FileReader(path));
+        String linha = "";
+        while (true) {
+            if (linha != null) {
+                System.out.println(linha);
+
+            } else
+                break;
+            linha = buffRead.readLine();
+        }
+        buffRead.close();
+    }
+
+    public static void escritor(String path) throws IOException {
+
+        ArrayList<String> arquivo = new ArrayList<>();
+
+        arquivo.add("Titulo");
+        System.out.println("");
+
+        BufferedWriter escrever = new BufferedWriter(new FileWriter(path));
+        String linha = "";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Escreva algo: ");
+        linha = in.nextLine();
+        escrever.append(linha + "\n");
+        linha = in.nextLine();
+        escrever.append(linha + "\n");
+        escrever.close();
+        System.out.println("Criado com sucesso");
+
+    }
+}
+
